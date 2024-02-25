8fe04763fc00fb138c406f29674900cec39e5524
armeenabbas <armeen.abbasali@gmail.com>
2024-02-25 11:53:46 +0500

    first commit

---
+++ src/BlogDetail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import Header from './Header'
+
+const BlogDetail = () => {
+  const { id } = useParams() 
+  return (
+    
+    <div>
+      <Header />
+      blog detail - {id}
+    </div>
+  )
+}
+
+export default BlogDetail
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import About from './About';
+import ErrorPage from './Error';
+import BlogDetail from './BlogDetail';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/about",
+    element: <About />,
+    errorElement: <ErrorPage />
+
+  },
+  {
+    path: "/blogs",
+    element: <App />,
+    errorElement: <ErrorPage />
+
+  },
+<<<<<<< HEAD
+{
+  path:`/blogs/:id`,
+  element:<BlogDetail/>,
+  errorElement:<ErrorPage/>
+
+}
+
+
+=======
+  {
+    path: `/blogs/:id`,
+    element: <BlogDetail />,
+    errorElement: <ErrorPage />
+  },
+>>>>>>> 43a476907c39f85162c0ba2e867f19ffd3c35da3
+
+
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
